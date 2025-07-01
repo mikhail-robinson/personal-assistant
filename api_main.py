24d241ad17e3f4ed57eeb881128826456776fea7
Mikhail Robinson <126931597+mikhail-robinson@users.noreply.github.com>
2025-07-01 14:58:53 +1200

    fix incorrect application name

--- api_main.py
+++ api_main.py
@@ -219,4 +219,4 @@ if __name__ == "__main__":
 
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api_main:app", host="0.0.0.0", port=8000, reload=True)
